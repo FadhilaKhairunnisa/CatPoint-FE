c35683f3b03b28d0f495ffdc01f956200bebae30
FadhillaKhairunnisa <20102319@ittelkom-pwt.ac.id>
2024-01-22 15:15:30 +0700

    fix minor middleware

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 Route::get('/register', [AuthController::class, 'indexRegister'])->name('auth.register');
 Route::get('/login', [AuthController::class, 'indexLogin'])->name('auth.login');
 Route::post('/login', [AuthController::class, 'loginProcess'])->name('auth.login.process');
 
-Route::get('/confirmation-payment/{id}', [HomeController::class, 'confirmationPayment'])->name('home.confirmation');
-
-Route::get('/generate/{trx_id}', [HomeController::class, 'generate'])->name('home.generate');
-Route::get('/success/{invoice_id}', [HomeController::class, 'success'])->name('home.success');
-Route::get('/invoice/{invoice_id}', [HomeController::class, 'invoice'])->name('home.invoice');
 Route::middleware(['authjwt'])->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home.index');
+    Route::get('/confirmation-payment/{id}', [HomeController::class, 'confirmationPayment'])->name('home.confirmation');
+
+    Route::get('/generate/{trx_id}', [HomeController::class, 'generate'])->name('home.generate');
+    Route::get('/success/{invoice_id}', [HomeController::class, 'success'])->name('home.success');
+    Route::get('/invoice/{invoice_id}', [HomeController::class, 'invoice'])->name('home.invoice');
     Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
     Route::get('/dashboard/jenispaket', [AdminController::class, 'jenispaket']);
     Route::get('/dashboard/pesanan', [AdminController::class, 'pesanan']);
     Route::get('/dashboard/invoice', [AdminController::class, 'invoice']);
     Route::get('/dashboard/testimoni', [AdminController::class, 'testimoni']);
 });
-
